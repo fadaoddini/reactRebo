145783cce709c6ff91cd45be5a2b56fa25868f2e
saeedfadaoddini <saeedfadaoddini@gmail.com>
2024-07-30 18:35:01 +0330

    three10

--- src/components/category/filtering.jsx
+++ src/components/category/filtering.jsx
@@ -3,10 +3,13 @@ import './filter.css';
 
 import topWeight from "../../assets/images/top_weight.png"
 import lowWeight from "../../assets/images/low_weight.png"
+import vertical from "../../assets/images/vertical.svg"
+import horizontal from "../../assets/images/horizontal.svg"
 
 
 const FilteringBazar = () => {
     const [activeTab, setActiveTab] = useState('weightHighToLow');
+    const [displayMode, setDisplayMode] = useState('card'); // 'card' or 'list'
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
@@ -17,22 +20,29 @@ const FilteringBazar = () => {
       <i className="bi-sort-down"></i>
       <span className="filter-tabs-title">مرتب‌سازی براساس:</span>
       <ul className="filter-tabs-list">
-        
-        
         <li className={`filter-tab ${activeTab === 'weightHighToLow' ? 'active' : ''}`}>
           <button className="filter-button" onClick={() => handleTabClick('weightHighToLow')}>
-          <img src={topWeight} alt=""/>
+            <img src={topWeight} alt=""/>
             <span>بیشترین وزن</span>
           </button>
         </li>
         <li className={`filter-tab ${activeTab === 'weightLowToHigh' ? 'active' : ''}`}>
           <button className="filter-button" onClick={() => handleTabClick('weightLowToHigh')}>
-          <img src={lowWeight} alt=""/>
+            <img src={lowWeight} alt=""/>
             <span>کمترین وزن</span>
           </button>
         </li>
       </ul>
+      <div className="display-mode-icons">
+        <button onClick={() => setDisplayMode('card')} className={`display-mode-button ${displayMode === 'card' ? 'active' : ''}`}>
+          <img src={vertical} alt="Card View" />
+        </button>
+        <button onClick={() => setDisplayMode('list')} className={`display-mode-button ${displayMode === 'list' ? 'active' : ''}`}>
+          <img src={horizontal} alt="List View" />
+        </button>
+      </div>
     </div>
+    
   );
 };
 
--- src/pages/bazar/index.jsx
+++ src/pages/bazar/index.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React from "react";
+import CardReboShop from "../../components/cards/cardReboShop";
+import CardWithSearch from "../../components/search/cardWhitSearch"
+import Switch from "../../components/switch/switch";
+import FilteringBazar from "../../components/category/filtering";
+import ImageCard from "../../components/imageCard/imageCard";
+import flag from "../../assets/images/flag.png";
+import mazafati from "../../assets/images/mazafati.jpg";
+import rabi from "../../assets/images/rabi.jpg";
+import darage2 from "../../assets/images/darage2.jpg";
 
 const Bazar = () => {
-    return ( 
-        <div>
-            <p> bazar</p>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-9">
+          <CardWithSearch />
+          <FilteringBazar />
+          <div className="card custom-card margin-top-5">
+            <div className="card-body">
+              <CardReboShop />
+            </div>
+          </div>
         </div>
-     );
-}
+        <div className="col-lg-3">
+          <div className="card custom-card margin-top-5 ">
+            <ImageCard
+              image={mazafati}
+              title="انواع خرمای مضافتی"
+              link="https://example.com"
+              icon={flag}
+            />
+            <ImageCard
+              image={rabi}
+              title="انواع خرمای ربی"
+              link="https://example.com"
+              icon={flag}
+            />
+            <ImageCard
+              image={darage2}
+              title="انواع خرمای درجه دو"
+              link="https://example.com"
+              icon={flag}
+            />
 
- 
+            <div className="card-body ">
+              <Switch title=" فروشنده" />
+              <Switch title=" خریدار" />
+              <Switch title=" ارسال رایگان" />
+              <Switch title="شرکتی" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Bazar;

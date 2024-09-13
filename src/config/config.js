6e94ceca1ca47e6c63a60f21dbcd54a2aed1b6e4
fadaoddini <saeedfadaoddini@gmail.com>
2024-09-13 22:02:43 +0330

    add shop 4

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
-    baseUrl: 'http://localhost:8000',
+    baseUrl: 'https://rebo.ir',
     apiVersion: 'v1',
-    // افزودن تنظیمات دیگر به اینجا
+
   };
   
   export default config;
